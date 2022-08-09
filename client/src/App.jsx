7def50be96d8b27f96d59979d7a1e6cfe4fd7436
nsn1930 <pavel.brodovskij@gmail.com>
2022-08-09 14:32:53 +0200

    back connect to front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import {  useEffect, useState } from 'react'
-import './App.scss';
+import './App.scss'
+import axios from 'axios'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api')
+    .then(res => {
+      setBackendData(res.data)
+    })
+  }, [])
+
   return (
     <div>
-
+      
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const cors = require("cors");
+app.use(cors());
+
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] })
 })
